759648d00473a493a21e7b2977ea528c7ad40579
Jose Pullaguari <jepullaguari@utpl.edu.ec>
2018-10-31 16:04:42 -0500

    Subiendo tarea de tutoria 04

---
+++ paquete/modelado.py
@@ -0,0 +1,40 @@
+#Creamos la clase object Persona
+class Persona(object):
+	#Definimos el constructor de la clase Persona
+	def __init__(self):
+		self.nombres = " "
+		self.edad = 0
+	#Metodos de agregar para los atributos
+	def agregar_nombres(self,n):
+		self.nombres = n
+
+	def agregar_edad(self,n):
+	    self.edad = n
+	#Metodos de obtener para los atributos
+	def obtener_edad(self):
+		return self.edad
+	#Metodo que presentamos datos
+	def presentar_datos(self):
+		c="%s-%s"%(self.obtener_edad(),self.nombres)
+		return c
+
+#Creamos la clase Estudiante con herencia de Persona
+class Estudiante(Persona):
+
+	#Definimos el constructor de la clase Estudiante
+	def __init__(self):
+		#Definimos la palabra super para heredar de la clase Persona
+		super(Estudiante,self).__init__()
+		self.nota = 0
+	#Metodo para agregar nota
+	def agregar_nota(self,n):
+		self.nota = n
+	#Metodo para obtener nota
+	def obtener_nota(self):
+		return nota
+	#Metodo para presentar datos con herencia
+	def presentar_datos(self):
+		#c="%s-%s-%s"%(self.nombres,self.edad,self,nota)
+		c="%s-%s"%(super(Estudiante,self).presentar_datos(),
+					self.nota)
+		return c
